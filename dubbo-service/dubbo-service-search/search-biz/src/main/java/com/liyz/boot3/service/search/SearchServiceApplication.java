33a4369b98f4ac8a06d7a6f09268aa0de28684ca
liyangzhen <812672598@qq.com>
2023-11-14 09:32:26 +0800

    add module : es-service

---
+++ dubbo-service/dubbo-service-search/search-biz/src/main/java/com/liyz/boot3/service/search/SearchServiceApplication.java
@@ -0,0 +1,19 @@
+package com.liyz.boot3.service.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Desc:
+ *
+ * @author lyz
+ * @version 1.0.0
+ * @date 2023/11/14 9:15
+ */
+@SpringBootApplication
+public class SearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchServiceApplication.class, args);
+    }
+}
