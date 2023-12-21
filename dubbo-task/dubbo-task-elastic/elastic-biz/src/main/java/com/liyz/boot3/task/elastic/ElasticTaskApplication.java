fcd749eec134efee88870780f0207d2ed7b4e03c
liyangzhen <812672598@qq.com>
2023-12-21 09:49:50 +0800

    add module:dubbo-task

---
+++ dubbo-task/dubbo-task-elastic/elastic-biz/src/main/java/com/liyz/boot3/task/elastic/ElasticTaskApplication.java
@@ -0,0 +1,19 @@
+package com.liyz.boot3.task.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Desc:
+ *
+ * @author lyz
+ * @version 1.0.0
+ * @date 2023/12/20 19:27
+ */
+@SpringBootApplication
+public class ElasticTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticTaskApplication.class);
+    }
+}
