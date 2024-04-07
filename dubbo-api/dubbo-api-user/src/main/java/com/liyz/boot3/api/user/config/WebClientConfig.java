4b52349c9d070ee99d82f522d8095118a17c2309
liyangzhen <812672598@qq.com>
2024-04-07 10:03:05 +0800

    test elk and filebeta

--- dubbo-api/dubbo-api-user/src/main/java/com/liyz/boot3/api/user/config/WebClientConfig.java
+++ dubbo-api/dubbo-api-user/src/main/java/com/liyz/boot3/api/user/config/WebClientConfig.java
@@ -3,6 +3,7 @@ package com.liyz.boot3.api.user.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.liyz.boot3.api.user.controller.test.SschaService;
 import lombok.SneakyThrows;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -17,14 +18,21 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
  * @date 2024/3/20 17:20
  */
 @Configuration
+@EnableConfigurationProperties(SscProperties.class)
 public class WebClientConfig {
+
+    private final SscProperties properties;
+
+    public WebClientConfig(SscProperties properties) {
+        this.properties = properties;
+    }
 
     @Bean
     public WebClient webClient(ObjectMapper objectMapper) {
         return WebClient.builder()
-                .baseUrl("https://openapi.sscha.com")
+                .baseUrl(properties.getBaseUrl())
                 .defaultHeaders(httpHeaders -> {
-                    httpHeaders.add("Authorization", "736fe7a1-6bc4-4663-b1f3-fc99aa1af2b0");
+                    httpHeaders.add(properties.getAuthHeader(), properties.getAuthValue());
                 })
                 .build();
     }
