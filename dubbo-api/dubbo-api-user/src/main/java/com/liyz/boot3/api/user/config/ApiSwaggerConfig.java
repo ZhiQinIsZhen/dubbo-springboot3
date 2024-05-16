d62da24d2a759823f5c4d6c77f40a81f5f4e149d
liyangzhen <812672598@qq.com>
2024-05-16 14:38:41 +0800

    optimize code: update swagger config

--- dubbo-api/dubbo-api-user/src/main/java/com/liyz/boot3/api/user/config/ApiSwaggerConfig.java
+++ dubbo-api/dubbo-api-user/src/main/java/com/liyz/boot3/api/user/config/ApiSwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.liyz.boot3.api.user.config;
 
 import com.liyz.boot3.common.api.config.SwaggerConfig;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -13,4 +15,48 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApiSwaggerConfig extends SwaggerConfig {
 
+    @Bean
+    public GroupedOpenApi allGroupedOpenApi() {
+        return GroupedOpenApi
+                .builder()
+                .group("全部")
+                .packagesToScan("com.liyz.boot3.api.user.controller")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi authGroupedOpenApi() {
+        return GroupedOpenApi
+                .builder()
+                .group("鉴权")
+                .packagesToScan("com.liyz.boot3.api.user.controller.authen")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi searchGroupedOpenApi() {
+        return GroupedOpenApi
+                .builder()
+                .group("搜索")
+                .packagesToScan("com.liyz.boot3.api.user.controller.search")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi userGroupedOpenApi() {
+        return GroupedOpenApi
+                .builder()
+                .group("用户")
+                .packagesToScan("com.liyz.boot3.api.user.controller.user")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi testGroupedOpenApi() {
+        return GroupedOpenApi
+                .builder()
+                .group("测试")
+                .packagesToScan("com.liyz.boot3.api.user.controller.test")
+                .build();
+    }
 }
