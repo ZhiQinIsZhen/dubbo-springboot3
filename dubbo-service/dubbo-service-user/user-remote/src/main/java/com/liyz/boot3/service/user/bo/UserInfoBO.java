d524d3bab270bb6349e702b2a31c1f3a60b18f53
liyangzhen <liyangzhen0114@foxmail.com>
2025-04-17 10:22:19 +0800

    Test: 1.Test redis lua

--- dubbo-service/dubbo-service-user/user-remote/src/main/java/com/liyz/boot3/service/user/bo/UserInfoBO.java
+++ dubbo-service/dubbo-service-user/user-remote/src/main/java/com/liyz/boot3/service/user/bo/UserInfoBO.java
@@ -20,17 +20,38 @@ public class UserInfoBO implements Serializable {
     @Serial
     private static final long serialVersionUID = 477985923412638468L;
 
+    /**
+     * 用户ID
+     */
     private Long userId;
 
+    /**
+     * 用户真实姓名
+     */
     private String realName;
 
+    /**
+     * 用户昵称
+     */
     private String nickName;
 
+    /**
+     * 手机号码
+     */
     private String mobile;
 
+    /**
+     * 邮箱地址
+     */
     private String email;
 
+    /**
+     * 安全盐值
+     */
     private String salt;
 
+    /**
+     * 注册时间
+     */
     private Date registryTime;
 }
